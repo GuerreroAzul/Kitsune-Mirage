6287b38698cb5241ce8f9d0beef8fce25c482d8c
GuerreroAzul <69611007+GuerreroAzul@users.noreply.github.com>
2025-02-18 14:39:17 -0600

    Update Test 7zip
    .

--- Applications/Accesories/7-zip-Test/Online/script.js
+++ Applications/Accesories/7-zip-Test/Online/script.js
@@ -1,7 +1,7 @@
 const OnlineInstallerScript = include("engines.wine.quick_script.online_installer_script");
 
 new OnlineInstallerScript()
-    .name("7-zip")
+    .name("7-zip-Test")
     .editor("Igor Pavlov")
     .applicationHomepage("http://www.7-zip.org/")
     .author("GuerreroAzul")
