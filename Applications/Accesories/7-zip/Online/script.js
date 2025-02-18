0c4f320b8f7c6534b58e761d8542059cb399585f
GuerreroAzul <69611007+GuerreroAzul@users.noreply.github.com>
2025-02-18 16:28:27 -0600

    Update script.js

--- Applications/Accesories/7-zip/Online/script.js
+++ Applications/Accesories/7-zip/Online/script.js
@@ -8,7 +8,4 @@ new OnlineInstallerScript()
     .url("https://www.7-zip.org/a/7z2409.exe")
     .checksum("2135a90a9f6c3202c32a87b1c5cf805ce294a497")
     .category("Accesories")
-    .preInstall(function (wine){
-        wine.run(this.url(), ["/S"]);
-    })
     .executable("7zFM.exe");
