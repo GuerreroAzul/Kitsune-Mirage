459d35fb3fd724b830b848c011da9935c1974a60
GuerreroAzul <69611007+GuerreroAzul@users.noreply.github.com>
2025-02-18 15:54:06 -0600

    Updtae 7zip Online
    Update Check SHA1

--- Applications/Accesories/7-zip/Online/script.js
+++ Applications/Accesories/7-zip/Online/script.js
@@ -6,6 +6,6 @@ new OnlineInstallerScript()
     .applicationHomepage("http://www.7-zip.org/")
     .author("GuerreroAzul")
     .url("https://versaweb.dl.sourceforge.net/project/sevenzip/7-Zip/24.09/7z2409.exe?viasf=1")
-    .checksum("00cbef9691efad7a56332fbcf51aa762")
+    .checksum("2135a90a9f6c3202c32a87b1c5cf805ce294a497")
     .category("Accesories")
     .executable("7zFM.exe");
