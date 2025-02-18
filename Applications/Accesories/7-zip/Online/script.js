e83446d630c1ede861170a4d5d28d4eaca105cfc
GuerreroAzul <69611007+GuerreroAzul@users.noreply.github.com>
2025-02-18 16:07:20 -0600

    Update 7zip Online
    Add Argument to executable for silent mode.

--- Applications/Accesories/7-zip/Online/script.js
+++ Applications/Accesories/7-zip/Online/script.js
@@ -8,5 +8,7 @@ new OnlineInstallerScript()
     .url("https://versaweb.dl.sourceforge.net/project/sevenzip/7-Zip/24.09/7z2409.exe?viasf=1")
     .checksum("2135a90a9f6c3202c32a87b1c5cf805ce294a497")
     .category("Accesories")
-    .executable("7zFM.exe")
-    .executableArguments(["/S"]);
+    .preInstall(function (wine) {
+        wine.run(this.url(), ["/S"]);
+    })
+    .executable("7zFM.exe");
