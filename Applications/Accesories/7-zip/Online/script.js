c67a3153e97a097b0c3349b204609cdc7d98cc7b
GuerreroAzul <69611007+GuerreroAzul@users.noreply.github.com>
2025-02-18 15:27:51 -0600

    Changes to 7zip
    Changes to the 7zip script and thumbnails updated.

--- Applications/Accesories/7-zip/Online/script.js
+++ Applications/Accesories/7-zip/Online/script.js
@@ -1,7 +1,7 @@
 const OnlineInstallerScript = include("engines.wine.quick_script.online_installer_script");
 
 new OnlineInstallerScript()
-    .name("7-zipTest")
+    .name("7-zip")
     .editor("Igor Pavlov")
     .applicationHomepage("http://www.7-zip.org/")
     .author("GuerreroAzul")
