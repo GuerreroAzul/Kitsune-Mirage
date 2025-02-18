4326d089828972c4df8010217e6cfc538956b205
GuerreroAzul <69611007+GuerreroAzul@users.noreply.github.com>
2025-02-18 14:50:05 -0600

    Update Test
    .

--- scripts/Applications/Accesories/7-zipTest/Online/script.js
+++ scripts/Applications/Accesories/7-zipTest/Online/script.js
@@ -1,11 +1,11 @@
 const OnlineInstallerScript = include("engines.wine.quick_script.online_installer_script");
 
 new OnlineInstallerScript()
-    .name("7-zip-Test")
+    .name("7-zipTest")
     .editor("Igor Pavlov")
     .applicationHomepage("http://www.7-zip.org/")
     .author("GuerreroAzul")
     .url("https://www.7-zip.org/a/7z2409.exe")
     .checksum("00cbef9691efad7a56332fbcf51aa762")
-    .category("Archiving")
+    .category("Accesories")
     .executable("7zFM.exe");
