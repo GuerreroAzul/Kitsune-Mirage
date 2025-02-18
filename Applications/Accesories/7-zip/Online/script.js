5833ee71a749743d640854932295be61eed55969
GuerreroAzul <69611007+GuerreroAzul@users.noreply.github.com>
2025-02-18 15:51:34 -0600

    Update 7zip Online
    Update URL Download,

--- Applications/Accesories/7-zip/Online/script.js
+++ Applications/Accesories/7-zip/Online/script.js
@@ -5,7 +5,7 @@ new OnlineInstallerScript()
     .editor("Igor Pavlov")
     .applicationHomepage("http://www.7-zip.org/")
     .author("GuerreroAzul")
-    .url("https://www.7-zip.org/a/7z2409.exe")
+    .url("https://versaweb.dl.sourceforge.net/project/sevenzip/7-Zip/24.09/7z2409.exe?viasf=1")
     .checksum("00cbef9691efad7a56332fbcf51aa762")
     .category("Accesories")
     .executable("7zFM.exe");
