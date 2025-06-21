873e19c881c746d129b7e011fb1eba1d37f9ecd8
GuerreroAzul <69611007+GuerreroAzul@users.noreply.github.com>
2025-06-21 12:44:26 -0600

    Actualizar CMR2

---
+++ Applications/Games/Colin McRae Rally 2/Local/script.js
@@ -0,0 +1,33 @@
+const LocalInstallerScript = include("engines.wine.quick_script.local_installer_script");
+const Resource = include("utils.functions.net.resource");
+
+new LocalInstallerScript()
+    .name("Colin McRae Rally 2")
+    .editor("Codemasters")
+    .applicationHomepage("https://www.ea.com/ea-studios/codemasters")
+    .author("GuerreroAzul")
+    .category("Games")
+    .wineVersion("9.0")
+    .wineArchitecture("x86")
+    .wineOs("win98")
+    .prefix("cmr2")
+    .executable("CMR2.EXE")
+    .postInstall(function (wine, wizard) {
+        // Recursos
+        wine.verb("d3dx9");
+        wine.verb("dinput8");
+
+        // Instalar CMR2 Official WRC Liveries
+        var option = wizard.menu("¿Deseas instalar el mod oficial WRC?", ["Sí", "No"]);
+        if (option === "Sí") {
+            var modPath = new Resource()
+                .wizard(wizard)
+                .url("https://archive.org/download/Game-POL/Colin%20McRae%20Rally%202/CMR2_Official_WRC_Liveries.exe")
+                .checksum("59e00d37450e39af3d6e2b7e7cdf2b77", "md5")
+                .name("CMR2_Official_WRC_Liveries.exe")
+                .get();
+
+            wine.run(modPath);
+            wizard.wait("Instalando CMR2 Official WRC Liveries...");    
+        }
+    });
